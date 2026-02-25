1502679056755f58bc94f59d73a014857d2f6c7e
Aytac <abalayeva12@gmail.com>
2026-02-25 17:11:33 +0400

    implement  a hoc managing window states and adding terminal window

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,16 @@
+import gsap from "gsap";
 import { Navbar, Welcome, Dock } from "./components";
+import { Draggable } from "gsap/draggable";
+import { Terminal } from "./Windows";
+
+gsap.registerPlugin(Draggable);
 const App = () => {
   return (
     <main>
       <Navbar />
       <Welcome />
       <Dock />
+      <Terminal/>
     </main>
   );
 };
